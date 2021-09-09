dfb4fa2a7699fd915aab733e248def3553bd3565
Mariano Forti <mariano.forti@icams.rub.de>
2021-09-09 23:24:55 +0200

    cleaning main loop in sublat sorter

--- SublatticeSorter.py
+++ SublatticeSorter.py
@@ -12,13 +12,21 @@ import numpy as np
 import re
 import sys
 from string import ascii_lowercase, ascii_uppercase
+import os
 from tqdm import tqdm
 
 if __name__ == '__main__':
     searchs = sys.argv[1]
 
-files = glob.glob('**/'+searchs, recursive=True)
-
+def get_file_paths( searchs, csvfile ='LIST_OF_files.csv'):
+    if os.path.exists(csvfile) and os.path.getmtime(__file__) < os.path.getmtime(csvfile):
+        with open('LIST_OF_files.csv', 'r') as f: 
+            files = f.readlines('LIST_OF_files.csv')
+    else:
+        files = glob.glob('**/'+searchs, recursive=True)
+        with open('LIST_OF_files.csv', 'w') as f:
+            f.writelines('\n'.join(files))
+    return files
 
 def get_sorted_poscar(poscar_orig: pd.core.series.Series, thissorter: list = [], _wheredirect: int=7):
     POSCAR_corrected = poscar_orig.copy()
@@ -40,42 +48,63 @@ def get_sorted_poscar(poscar_orig: pd.core.series.Series, thissorter: list = [],
         POSCAR_corrected.iloc[np.sort(thissorter)]= poscar_orig.iloc[ thissorter ]
     else:
         POSCAR_corrected.iloc[_wheredirect+1:]= poscar_orig.iloc[_wheredirect+1:]
-
     return POSCAR_corrected
-
-SORTERS = {}
-SUBLATICETAGS = {}
 
 def get_real_sublattice(string):
     if len(string) > 0:
         return string[0]
     else:
         return ''
+def get_coord_type_line(thisposcar):
+    return thisposcar[thisposcar.str.contains('\s*dir|\s*CAR', regex = True, flags=re.IGNORECASE)].index.values[0]
 
-#TODO: make function and check existence of pickle.
+def get_number_of_atoms(thisposcar, num_line_coord_type):
+    return sum( [ int(n) for n in  re.split('\s+', thisposcar[num_line_coord_type-1].strip()) ] )
 
-for thisfile in tqdm(files):
-    POSCAR = pd.read_csv(thisfile, sep='\n', dtype='str', header=None)[0]
-    wheredirect = POSCAR[POSCAR.str.contains('\s*dir|\s*CAR', regex = True, flags=re.IGNORECASE)].index.values[0]
+def get_sorted_sites_indexes(thisposcar):
     SORTER = []
-    natoms = sum( [ int(n) for n in  re.split('\s+', POSCAR[wheredirect-1].strip()) ] )
     for SITE in ascii_uppercase:
-        SORTER +=  POSCAR[POSCAR.str.contains('[0-9]\s+'+SITE, regex=True)].index.tolist()
-    POSCAR_SORTED = get_sorted_poscar(POSCAR, SORTER, _wheredirect=wheredirect)
-    POSCAR_SORTED.to_csv(thisfile+'-sorted', sep='\n', index=False, header=None)
-#    SUBLATICETAGS[thisfile] = POSCAR_SORTED[wheredirect+1:natoms].map( lambda l: re.findall('[A-Z]',l)).values
-    SUBLATICETAGS[thisfile]=POSCAR_SORTED[wheredirect+1:wheredirect+1+natoms].str.findall('[A-Z]').map(get_real_sublattice).values
+        SORTER +=  thisposcar[thisposcar.str.contains('[0-9]\s+'+SITE, regex=True)].index.tolist()
+    return SORTER
+def get_sublatticetags_sorted(thisposcarsorted):
+    return POSCAR_SORTED[wheredirect+1:wheredirect+1+natoms].str.findall('[A-Z]').map(get_real_sublattice).values
 
-    thisfile_relax = thisfile.replace('-initial','-relaxed').replace('.initial','.relaxed-all')
-    try:
-        POSCAR_RLX = pd.read_csv(thisfile_relax, sep='\n', dtype='str', header=None)[0]
-#        wheredirect_rlx = POSCAR[POSCAR.str.contains('\s*dir|\s*CAR', regex = True)].index.values[0]
-        POSCAR_RLX_SORTED = get_sorted_poscar(POSCAR_RLX, SORTER, _wheredirect=wheredirect)
-        POSCAR_RLX_SORTED.to_csv(thisfile_relax+'-sorted', index=False, sep='\n', header=None)
-        SORTERS[thisfile_relax]=SORTER
-    except FileNotFoundError as E:
-        pass
-        #print('relaxation nof found for '+thisfile)
-SORTERS = pd.Series(SORTERS)
-SUBLATICETAGS = pd.Series(SUBLATICETAGS)
+def get_sorter_and_sorted_tags(_thisfile, THISSORTER=None, THISSUBLATICETAGS=None):
+    TORETURN = ()
+    POSCAR = pd.read_csv(_thisfile, sep='\n', dtype='str', header=None)[0]
+    wheredirect = get_coord_type_line(POSCAR)
+    natoms = get_number_of_atoms(POSCAR, wheredirect)
+    if THISSORTER==None: # return sorter
+        TORETURN += ( get_sorted_sites_indexes(POSCAR), )
+    POSCAR_SORTED = get_sorted_poscar(POSCAR, THISSORTER, _wheredirect=wheredirect)
+    POSCAR_SORTED.to_csv(_thisfile+'-sorted', sep='\n', index=False, header=None)
+    if THISSUBLATICETAGS==None:
+        TORETURN+=(get_sublatticetags_sorted(POSCAR_SORTED), )
+    return TORETURN
+
+def get_list_poscars(files):
+    SORTERS = {}
+    SUBLATICETAGS = {}
+
+    for thisfile in tqdm(files):
+        SORTER[thisfile], SUBLATICETAGS[thisfile]=get_sorter_and_sorted_tags (thisfile)
+
+        thisfile_relax = thisfile.replace('-initial','-relaxed').replace('.initial','.relaxed-all')
+        try:
+
+ #           POSCAR_RLX = pd.read_csv(thisfile_relax, sep='\n', dtype='str', header=None)[0]
+ #           wheredirect_rlx = get_coord_type_line(POSCAR_RLX) #POSCAR[POSCAR.str.contains('\s*dir|\s*CAR', regex = True)].index.values[0]
+ #           natoms_rlx = get_number_of_atoms(POSCAR_RLX, wheredirect_rlx)
+ #           POSCAR_RLX_SORTED = get_sorted_poscar(POSCAR_RLX, SORTER, _wheredirect=wheredirect_rlx)
+ #           POSCAR_RLX_SORTED.to_csv(thisfile_relax+'-sorted', index=False, sep='\n', header=None)
+ #           SORTERS[thisfile_relax]=SORTER
+ #           THISSUBLATTICETAGS=
+        except FileNotFoundError as E:
+            pass
+            #print('relaxation nof found for '+thisfile)
+
+    return pd.Series(SORTERS), pd.Series(SUBLATICETAGS)
+
+files = get_file_paths(searchs)
+SORTERS, SUBLATICETAGS = get_list_poscars(files)
 print('could sort only ', len(SORTERS),' of ',len(files))
