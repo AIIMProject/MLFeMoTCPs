3d2ae51a6e3e57625f0f54771b405555aa8377c5
Mariano Forti <mariano.forti@icams.rub.de>
2021-10-07 07:49:40 +0200

    moduled Plotting and Naming functions

--- TestCutoffForNeighbours.py
+++ TestCutoffForNeighbours.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import sys
 from itertools import product
 from BopFoxFeaturizer.Featurizer import Featurizer, BopfoxFeatures
-from SourceDevelopementVersion import StructSummaryParser, Featurizer, BopfoxFeatures
+#from SourceDevelopementVersion import StructSummaryParser, Featurizer, BopfoxFeatures
 
 cutoffs = { 'table': 'TABLECUTOFF','histogram': 'HISTCUTOFF' }
 models = { 'canonicalTB':'CANONICAL', 'orthogonal_TB': 'ORTHOGONAL' }
@@ -20,13 +20,14 @@ atoms = {'initial': 'INITIAL', 'relaxed':'RELAXED'}
 for keymodel, keyatoms, keycutof in product(models.keys(), atoms.keys(), cutoffs.keys()):
     print('atoms: ', atoms[keyatoms], 'model: ', models[keymodel], '  cutoff: ', cutoffs[keycutof])
     atomspickle = f'CrCoW-sorted-POSCAR-{keyatoms}-rescaled-AtomsObjects.pkl'
-    resultspickle = f'CRCOW_{atoms[keyatoms]}_NSC_{models[keymodel]}_{cutoffs[keycutof]}.pkl'
+    resultspickle = f'CRCOW_{atoms[keyatoms]}_NSC_{models[keymodel]}_{cutoffs[keycutof]}_WENERGY.pkl'
     AtomsObjects = pd.read_pickle(atomspickle).dropna(how='any')
     BOPC = BopfoxFeatures(
             AtomsObjects['atoms'],modelsfile[keymodel], modelname=keymodel,
-            cutoffby='table'
+            cutoffby='table', 
+            binary = '/home/storage/fortimtb/CuadernoTrabajo/oldrepobopfox/src/bopfox_mpi'
             )
     BOPC.calculate_bop_forall(ForceKeepSpecies=True,
-            input_pickle = resultspickle,
+            input_pickle = resultspickle
             )
     BOPC.RESULTS.to_pickle(resultspickle)
