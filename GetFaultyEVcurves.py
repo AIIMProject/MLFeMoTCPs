a14d78a3b1bfedcabcd8ac034f0d4bc8439ce9d7
Mariano Forti <mariano.forti@icams.rub.de>
2022-02-09 16:56:13 +0100

    can generate pdf with also v0 filter

--- GetFaultyEVcurves.py
+++ GetFaultyEVcurves.py
@@ -50,32 +50,50 @@ def plot_fitted_curve(evcurve, thefit, r2, fig=None,  ax=None):
         pass
     return l
 
-
-if __name__ == '__main__':
+def get_goodness(EVcurves):
     goodness = {}
-    EVcurves = pd.read_json('evcurves.json', orient='index')
+    fiteos = {}
+    r2 = {}
     progress = tqdm(EVcurves.iterrows(), total = len(EVcurves))
-#    with PdfPages('multipage_fitted_curves.pdf') as pdf:
-#        with PdfPages('bad_evcurves_multipage.pdf') as badpdf:
     for thisid, curvedata in progress:
-#        fig, ax = plt.subplots(1,1)
-#            plotted_lines = {}
         goodness[thisid] =  {}
+        fiteos[thisid] = {}
+        r2[thisid] = {}
         for key in curvedata['evcurve'].keys():
-            fiteo, r2 = get_eos_goodness(curvedata['evcurve'][key])
-#                plotted_lines.append(plot_fitted_curve (curvedata['evcurve'][key], fiteo, r2, fig=fig, ax=ax))
-            if  r2 < 0.99:
+            fiteos[thisid][key], r2[thisid][key] = get_eos_goodness(curvedata['evcurve'][key])
+            if fiteos[thisid][key] is None:
+                goodness[thisid].update({ key: False })
+                continue
+            v0 = fiteos[thisid][key][-1]
+            vmax = np.max( curvedata['evcurve'][key]['V'] )
+            vmin = np.min( curvedata['evcurve'][key]['V'] )
+            if  r2[thisid][key] < 0.99 or (v0 < vmin or v0 > vmax):
                 goodness[thisid].update({ key: False })
             else:
                 goodness[thisid].update({ key: True })
-#           if len(plotted_lines) > 0:
-#               ax.legend()
-#               fig.suptitle(thisid)
-#               fig.tight_layout()
-#           plt.close(fig)
     df = pd.Series(goodness)
     df.to_json('goodness.json')
+    return df, fiteos, r2
     
-    
-
-
+if __name__ == '__main__':
+    EVcurves = pd.read_json('evcurves.json', orient='index')
+    goodness, fiteos, r2  = get_goodness(EVcurves)         
+    progress = tqdm(EVcurves.iterrows(), total = len(EVcurves))
+    with PdfPages('multipage_fitted_curves.pdf') as pdf:
+#        with PdfPages('bad_evcurves_multipage.pdf') as badpdf:
+        for thisid, curvedata in progress:
+            if any(goodness[thisid].values()):
+                fig, ax = plt.subplots(1,1)
+                plotted_lines = []
+                for key, thisdata in curvedata['evcurve'].items():
+                    if goodness[thisid][key]:
+                        plotted_lines += plot_fitted_curve (curvedata['evcurve'][key], fiteos[thisid][key],r2[thisid][key],  fig=fig, ax=ax)
+                try:
+                    ax.legend()
+                except Exception as E:
+                    pdb.set_trace()
+                    pass
+                fig.suptitle(thisid)
+                fig.tight_layout()
+                pdf.savefig(fig)
+                plt.close(fig)
