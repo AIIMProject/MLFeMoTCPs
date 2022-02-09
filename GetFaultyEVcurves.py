6407b27a654fac0fbf80bea719be9bb35aa989ef
Mariano Forti <mariano.forti@icams.rub.de>
2022-02-09 14:22:34 +0100

    faulty evcurves filter to json

--- GetFaultyEVcurves.py
+++ GetFaultyEVcurves.py
@@ -3,62 +3,78 @@ import pandas as pd
 from ase.eos  import EquationOfState, birchmurnaghan
 import numpy as np
 from tqdm import tqdm
-from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
+import warnings
 
 
 
 def get_eos_goodness(thecurve):
-    try:
-        V0 = thecurve['ev_fit_results']['V_murn']
-        E0 = thecurve['ev_fit_results']['E_murn']
-        B0 = thecurve['ev_fit_results']['B_murn']
-        B0p = thecurve['ev_fit_results']['Bdev_murn']
-        results, pcov = curve_fit(birchmurnaghan, thecurve['V'], thecurve['E'], [E0, B0, B0p, V0] )
-    except Exception as E:
+    if len(thecurve['ev_fit_results']) > 0:
+        V0 = float(thecurve['ev_fit_results']['V_murn'])
+        E0 = float(thecurve['ev_fit_results']['E_murn'])
+        B0 = float(thecurve['ev_fit_results']['B_murn'])
+        B0p = float(thecurve['ev_fit_results']['Bdev_murn'])
+        try:
+            results, pcov = curve_fit(birchmurnaghan, thecurve['V'], thecurve['E'], [E0, B0, B0p, V0] )
+            e =  birchmurnaghan(thecurve['V'], results[0], results[1], results[2], results[3])
+        except RuntimeError as RE:
+            results = [np.mean( thecurve['E'] ),  1e36, 1e36, np.mean( thecurve['V'] )]
+            e = [np.mean(thecurve['E'])]*len(thecurve['V'])
+        except Exception as E:
+            pdb.set_trace()
+            pass
+        r2 = r2_score(thecurve['E'], e)
+    else:
         results = None
-        pcov = [np.Inf]
-    return results, pcov
+        r2 = -np.Inf
+    return results, r2
     
-def plot_fitted_curve(evcurve, thefit, thecov, fig=None,  ax=None):
-    try:
-        v = np.linspace(min(evcurve['V']), max(evcurve['V']), 100)
-        e =  birchmurnaghan(v, thefit[0], thefit[1], thefit[2], thefit[3])
-        l = ax.plot(v,e,'-', label = 'fit')[0]
-        thecovlabel = ' '.join([f'{cov/val:.1e}' for cov, val  in zip(np.diag(thecov), thefit)])
-        ax.plot(evcurve['V'], evcurve['E'], 'o', color=l.get_color(), label=thecovlabel)
+def plot_fitted_curve(evcurve, thefit, r2, fig=None,  ax=None):
+    l = []
+    if thefit is not None:
+        try: 
+            v = np.linspace(min(evcurve['V']), max(evcurve['V']), 100)
+            e =  birchmurnaghan(v, thefit[0], thefit[1], thefit[2], thefit[3])
+        except Exception as E:
+            pdb.set_trace()
+            pass
+        l.append(ax.plot(v,e,'-', label = 'fit')[0])
+        thecovlabel = f'$R^2 = $ {r2:.3f}' 
+        l.append(ax.plot(evcurve['V'], evcurve['E'], 'o', color=l[-1].get_color(), label=thecovlabel))
         fig.tight_layout()
-    except TypeError as E:
-        print(E)
+    else:
         pass
-    except ValueError as E:
-        print(E)
-        pass
+    return l
+
 
 if __name__ == '__main__':
     goodness = {}
     EVcurves = pd.read_json('evcurves.json', orient='index')
     progress = tqdm(EVcurves.iterrows(), total = len(EVcurves))
-    with PdfPages('multipage_fitted_curves.pdf') as pdf:
-        with PdfPages('bad_evcurves_multipage.pdf') as badpdf:
-            for id, curvedata in progress:
-                goodness[id]={}
-                fig, ax = plt.subplots(1,1)
-                pcov = []
-                for key in curvedata['evcurve'].keys():
-                    fiteo, tpcov = get_eos_goodness(curvedata['evcurve'][key])
-                    pcov.append(np.diag(tpcov))
-                    goodness[id][key]=np.sqrt(np.diag(tpcov).sum())
-                    plot_fitted_curve (curvedata['evcurve'][key], fiteo, tpcov, fig=fig, ax=ax)
-                ax.legend()
-                if len(tpcov) < 2 or abs(pcov[-1][-2]/fiteo[-2]) > 10:
-                    badpdf.savefig(fig)
-                else:
-                    pdf.savefig(fig)
-                plt.close(fig)
+#    with PdfPages('multipage_fitted_curves.pdf') as pdf:
+#        with PdfPages('bad_evcurves_multipage.pdf') as badpdf:
+    for thisid, curvedata in progress:
+#        fig, ax = plt.subplots(1,1)
+#            plotted_lines = {}
+        goodness[thisid] =  {}
+        for key in curvedata['evcurve'].keys():
+            fiteo, r2 = get_eos_goodness(curvedata['evcurve'][key])
+#                plotted_lines.append(plot_fitted_curve (curvedata['evcurve'][key], fiteo, r2, fig=fig, ax=ax))
+            if  r2 < 0.99:
+                goodness[thisid].update({ key: False })
+            else:
+                goodness[thisid].update({ key: True })
+#           if len(plotted_lines) > 0:
+#               ax.legend()
+#               fig.suptitle(thisid)
+#               fig.tight_layout()
+#           plt.close(fig)
+    df = pd.Series(goodness)
+    df.to_json('goodness.json')
     
     
 
