0abc2ded3e366d8d111e54d56adccfc674e2f07d
Mariano Forti <mariano.forti@icams.rub.de>
2022-04-02 14:39:22 +0200

    introducing pyscal featurzers

---
+++ Tools/DatasetTools/pyscalfeaturizers.py
@@ -0,0 +1,65 @@
+# # Pyscal features 
+
+# In[1]:
+
+
+import pyscal as pc
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+from tqdm.notebook import tqdm_notebook
+from time import time
+import pickle
+import pdb
+tqdm_notebook.pandas()
+
+CNS = {'CN12': 12, 'CN14': 14, 'CN15': 15, 'CN16': 16}
+
+def load_pyscal(thisatoms):
+    thesys = pc.System()
+    thesys.read_inputfile(thisatoms, format='ase')
+    return thesys
+
+def get_sys_cn(thissys):
+    thissys.find_neighbors(method='cutoff', cutoff = 'adaptive')
+    return [atom.coordination for atom in thissys.atoms]
+
+def get_sys_steinhardt(thissys):
+    thissys.find_neighbors(method='cutoff', cutoff = 0)
+    thissys.calculate_q([4,6])
+    return thissys.get_qvals([4,6])
+
+def get_cn(theatoms):
+    thissys = load_pyscal(theatoms)
+    return get_sys_cn(thissys)
+
+def get_steinhardt(theatoms):
+    thesys = load_pyscal(theatoms)
+    return get_sys_steinhardt(thesys)
+
+from tqdm.contrib.concurrent import process_map
+
+def timeit(func):
+    def inner(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        print( time() - t1 )
+        return result
+    return inner
+
+def featurize_dataframe(df, colid='atoms', featurizer=get_cn, max_workers=3):
+    cz = int(len(df) / max_workers)
+    print(featurizer.__name__)
+    Result = process_map(featurizer, df[colid],  max_workers = max_workers, chunksize=1)
+    return Result
+
+def featurize_many(AtomsObjects, featurizerlist):
+    Features = {thisfeaturizer.__name__: featurize_dataframe(AtomsObjects,featurizer=thisfeaturizer, max_workers=3) for thisfeaturizer in featurizerlist}
+    return Features
+
+if __name__=='__main__':
+    AtomsObjects = pd.read_pickle('Cr-Co-W/CrCoW-sorted-POSCAR-initial-rescaled-AtomsObjects.pkl').dropna()
+    featurizers = [get_cn, get_steinhardt]
+    Features = featurize_many(AtomsObjects, featurizers)
+    with open('pyscal_features.kpl','wb') as pkl:
+        pickle.dump(Features, pkl)
