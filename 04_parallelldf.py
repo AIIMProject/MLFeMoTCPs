128b724dbae6e49f5561e3857adb1ef413b93288
Mariano Forti <mariano.forti@icams.rub.de>
2022-04-02 09:35:07 +0200

    can featurize_many from pyscan

--- 04_parallelldf.py
+++ 04_parallelldf.py
@@ -5,16 +5,13 @@
 
 import pyscal as pc
 import pandas as pd
-from tqdm.auto import tqdm
 import numpy as np
 import multiprocessing as mp
 from tqdm.notebook import tqdm_notebook
 from time import time
+import pickle
 import pdb
 tqdm_notebook.pandas()
-
-
-
 
 CNS = {'CN12': 12, 'CN14': 14, 'CN15': 15, 'CN16': 16}
 
@@ -27,12 +24,20 @@ def get_sys_cn(thissys):
     thissys.find_neighbors(method='cutoff', cutoff = 'adaptive')
     return [atom.coordination for atom in thissys.atoms]
 
+def get_sys_steinhardt(thissys):
+    thissys.calculate_q([4,6])
+    return thissys.get_qvals([4,6])
+
 def get_cn(theatoms):
     thissys = load_pyscal(theatoms)
     return get_sys_cn(thissys)
 
-def featurize_row (theatoms):
-    return get_cn(theatoms)
+def get_steinhardt(theatoms):
+    thesys = load_pyscal(theatoms)
+    return get_sys_steinhardt(thesys)
+
+def featurize_row (theatoms, atomfeaturizer = [get_cn]):
+    return atomfaturizer(theatoms)
     
 
 from tqdm.contrib.concurrent import process_map
@@ -47,9 +52,14 @@ def timeit(func):
 
 def featurize_dataframe(df, colid='atoms', featurizer=get_cn, max_workers=3):
     cz = int(len(df) / max_workers)
+    print(featurizer.__name__)
     Result = process_map(featurizer, df[colid],  max_workers = max_workers, chunksize=1)
     return Result
 
 if __name__=='__main__':
     AtomsObjects = pd.read_pickle('Cr-Co-W/CrCoW-sorted-POSCAR-initial-rescaled-AtomsObjects.pkl').dropna()
-    CNS =  featurize_dataframe(AtomsObjects, max_workers=3)
+    CNS = {}
+    featurizers = [get_cn, get_steinhardt]
+    CNS =  {thisfeaturizer.__name__: featurize_dataframe(AtomsObjects,featurizer=thisfeaturizer, max_workers=3) for thisfeaturizer in featurizers}
+    with open('pyscal_features.kpl','wb') as pkl:
+        pickle.dump(CNS, pkl)
