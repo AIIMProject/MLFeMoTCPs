f2221f060476991996f6717434182d875daedb38
Mariano Forti <mariano.forti@icams.rub.de>
2022-04-03 14:30:10 +0200

    uses parallell feature calculation fully

--- Tools/DatasetTools/GetBopFeatures.py
+++ Tools/DatasetTools/GetBopFeatures.py
@@ -29,6 +29,7 @@ import sys
 sys.path.insert(0, '/home/storage/fortimtb/CuadernoTrabajo/bopfoxfeaturizer/')
 from BopFoxFeaturizer.Featurizer import Featurizer, BopfoxFeatures
 import pandas as pd
+import pdb 
 
 
 # ## options:
@@ -57,132 +58,30 @@ atomspickle =  os.path.join(dataset, f'{components}-sorted-POSCAR-{atoms}-rescal
 
 AtomsObjects = pd.read_pickle(atomspickle).dropna(how='any')
 
+from tqdm.contrib.concurrent import process_map
+from copy import deepcopy
 
-# In[5]:
+def inspect(compound):
+    print(compound)
+    compcopy = deepcopy(compound)
+    return compcopy
 
-
-from time import time
-
-
-# In[6]:
-
+#result = process_map(inspect, AtomsObjects['atoms'].iteritems())
 
 results = {}
+
 for model in models:
     modelsfile = os.path.join('models', f'{components}_{model}.bx')
     print('atoms: ', atoms, 'model: ', model, '  cutoff: ', cutoff, ' moments:', moments)
-    resultspickle = os.path.join(dataset, 'Descriptors', f'{components}_{atoms}_{model}_{cutoff}_WUBIND_{moments}.pkl')
+    resultspickle = os.path.join(dataset, 'Descriptors', f'paralell_{components}_{atoms}_{model}_{cutoff}_WUBIND_{moments}.pkl')
     BOPC = BopfoxFeatures(
             AtomsObjects['atoms'],modelsfile, modelname=model,
             cutoffby=cutoff, 
             binary = '/home/storage/fortimtb/CuadernoTrabajo/oldrepobopfox/src/bopfox_mpi',
             moments = moments
             )
-    BOPC.calculate_bop_forall(ForceKeepSpecies=True,
-            input_pickle = resultspickle
-            )
+    BOPC.featurize_dataframe(input_pickle=resultspickle, output_pickle=resultspickle)
     results[model] = BOPC.RESULTS
     BOPC.RESULTS.to_pickle(resultspickle)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-upper = {}
-
-
-# In[ ]:
-
-
-lower1 = {1: {'a': 'lista'}}
-lower2 = {1: {'b': 'listb'}}
-
-
-# In[ ]:
-
-
-upper.update(lower1)
-
-
-# In[ ]:
-
-
-upper.update(lower2)
-
-
-# In[ ]:
-
-
-upper
-
-
-# In[ ]:
-
-
-results['canonical']['sigma']
-
-
-# In[ ]:
-
-
-import re
-
-
-# In[ ]:
-
-
-import tarfile
-
-
-# In[ ]:
-
-
-import gzip
-
-
-# In[ ]:
-
-
-with tarfile.open('bopfoxASE_Co10Cr8W6_2022-03-31_11:22:11gmt.out.tar.gz','r') as thistar:
-    logbx = thistar.extract('log.bx')
-    with open('log.bx', 'r') as f:
-        loglines = f.readlines()
-
-
-# In[ ]:
-
-
-sigmalines = [re.match(' sigma \( atom = (.*), orbital = (.*)\) (.*)', logline) for logline in loglines]
-
-
-# In[ ]:
-
-
-import numpy as np
-
-
-# In[ ]:
-
-
-sigmas = {}
-for (atom, orbital, thissigmas) in [sigmaline.groups() for sigmaline in sigmalines if sigmaline != None]:
-    sigmas[atom+' '+orbital] = np.fromstring(thissigmas.strip(), sep=' ')
-
-
-# In[ ]:
-
-
-sigmas
-
-
-# In[ ]:
-
-
 
 
