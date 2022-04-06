c614591654bd0d8282be98e67eb157cf5f419e90
Mariano Forti <mariano.forti@icams.rub.de>
2022-04-06 13:14:38 +0200

    clean runs of notebooks up to 6, general featurizer includes cnaverages

--- Tools/DatasetTools/GeneralFeaturizer.py
+++ Tools/DatasetTools/GeneralFeaturizer.py
@@ -3,8 +3,6 @@ import pandas as pd
 from tqdm.auto import tqdm
 import pdb
 
-
-
 neighbours  = {'CN12': 12, 'CN14': 14, 'CN15': 15, 'CN16': 16}
 
 def cn_average(vectorfeature, coordination): # *args): #iterable, coordinations, axis=1):
@@ -13,17 +11,43 @@ def cn_average(vectorfeature, coordination): # *args): #iterable, coordinations,
     _, coord = coordination
     natoms = len(atomarray)
     for polyhedra, nneighbours in neighbours.items():
-        average['_0'] = atomarray.sum()/natoms
-        average[f'_{polyhedra}'] = atomarray[np.array(coord) == nneighbours].sum()/natoms
+        average['_0'] = np.sum(atomarray)/natoms
+        average[f'_{polyhedra}'] = np.array(atomarray)[np.array(coord) == nneighbours].sum()/natoms
     AveragedFeatures  = {index: average}
     return AveragedFeatures
 
 def cnaverage_dataframe(_Features, colids, _Coordinations, nworkers = 3):
     result = {}
-    for colid in colids:
-        print(f'cnaverage {colid}')
-        progress = tqdm(zip(_Features[colid].iteritems(), _Coordinations.iteritems()), total=_Features.shape[0])
-        thisresult = [cn_average(atomfeature, atomcoordinations) for atomfeature, atomcoordinations in progress]
+    progress = tqdm(colids)
+    for colid in progress:
+        progress.set_description(colid)
+        iterator  = zip(_Features[colid].iteritems(), _Coordinations.iteritems())
+        thisresult = [cn_average(atomfeature, atomcoordinations) for atomfeature, atomcoordinations in iterator]
         thisresult =  dict(map(dict.popitem, thisresult) )
         result.update({colid: pd.DataFrame.from_dict(thisresult,orient='index' )})
     return  result
+
+def get_dimensions(thefeature):
+    theshape = thefeature.shape
+    if len(theshape) == 2:
+        dimensions = theshape
+    elif len(theshape) == 1:
+        dimensions = (theshape, 1)
+    return dimensions
+
+def cnav_column_name_fix (dict_of_features):
+    for key, features in dict_of_features.items():
+        col_replacements = {col: f'{key}{col}' for col in features.columns}
+        features.rename(columns=col_replacements, inplace=True)
+        
+def array_expansions(Features, columnstoexpand):
+    df = {}
+    for feature in columnstoexpand:
+        df[feature] = {}
+        for index, compound in Features[feature].iteritems():
+            m = np.array(compound)
+            natoms, nm = get_dimensions(m)
+            df[feature][index] = {f'{feature}_{this}' :  m[:,this] for this in range(1,nm)}
+        df[feature] = pd.DataFrame.from_dict(df[feature], orient='index')
+    return pd.concat(df.values(), axis=1)
+
