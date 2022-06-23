cabdfe01405cce3c18ff9fab3e29c696ea9cd161
Mariano Forti <mariano.forti@icams.rub.de>
2022-06-23 15:55:18 +0200

    easy outlier detection by eye

---
+++ Tools/DatasetTools/Commoms.py
@@ -0,0 +1,16 @@
+""" 
+This is just a convenience tool for importing the same group of modules in the notebooks
+"""
+import os
+import sys
+import re
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+plt.style.use('default')
+plt.rc('figure', figsize=(15,10))
+plt.rc('font', size=22)
+import pdb
+from itertools import product
+
