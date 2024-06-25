281c509fd21301b4204eaed4c92b118c62386b0c
Mariano Forti <mariano.forti@icams.rub.de>
2024-06-25 14:01:13 +0200

    renew get bop features

--- Tools/DatasetTools/GetBopFeatures.py
+++ Tools/DatasetTools/GetBopFeatures.py
@@ -1,91 +1,202 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# #  Computation of BOP Features
-# 
-# - input: atoms objects
-# - output here: all the descriptors comming from BOP (averages, atomic site based, etc)
-# 
-# #  Calculation of features from available libraries
-# 
-# - input curated BS
-# 
-# 
-# 
-# **TODO**
-# - Imports
-# - make onefile for each model for each dataset: CrCoW_canonical.bx, CrCoW_projection.bx, CrCoW_projection_os.bx
-# - in the file : model = canonical
-# - files location in function of the dataset
-# - **options: dataset, model** #,  dataset+model -> modelsfile name + modelsfile section : one modelfile per dataset.
-# - generate modelsname from dataset+model
-# - TODO: remove s related params from models files: onsite levels and bond integrals
-
-# In[1]:
-
-
-import os
 import sys
+import os
+sys.path.insert(0, os.path.dirname(( os.path.dirname(os.path.dirname(__file__)) )))
+from Tools.DatasetTools.Commoms import *
+os.environ['PATH']+=':'+os.path.join(os.getcwd(),'dependencies/bopfox/src/')
 from dependencies.bopfoxfeaturizer.BopFoxFeaturizer.Featurizer import Featurizer, BopfoxFeatures
-import pandas as pd
-import pdb 
+
+from dependencies.bopdftprojections.bopdftprojections.projections import Projections, Canonicalmodel
+import shutil
 
 
-# ## options:
+from Tools.DatasetTools import GeneralFeaturizer as gf
+from sklearn.feature_selection import VarianceThreshold
+from tqdm.auto import tqdm
 
-# In[2]:
-
-
-dataset = 'Fe-Mo' 
-components = dataset.replace('-','')
-#models = ['canonical','projections', 'projections_os', 'projections_sos']
-#models =  ['projections_os'] #
-models = ['0.5projections_os', '0.6projections_os','0.7projections_os', '0.8projections_os', 'projections_os']
-cutoff = 'table'
-atoms = 'initial'
-moments = 16
 
 
 # In[3]:
 
+dataset = 'Fe-Mo' #  'Cr-Co-W'# 'Fe-Mo'
+elements = dataset.split('-')
+atomsobjectloc = os.path.join(dataset, 'Atomsobjects')
+components = dataset.replace('-','')
+globalmoments = 16
+model_definitions = {
+    '0.7projections_0.5os': {
+        'model_maker_options' : {
+            'element_pairs_kwargs' : {
+                'bond_integral_scale': 0.7,
+            },
+            'atom_blocks_kwargs': {
+                'onsite_levels_scale' : 0.5,
+                'select_orbitals' : {'Fe': 'd', 'Mo' : 'd'}
+            },
+        },
+        'calculator_options' : {
+            'moments' : globalmoments
+        }
+    },
+    '0.7projections_0.5os_10scf': {
+        'model_maker_options' : {
+            'element_pairs_kwargs' : {
+                'bond_integral_scale': 0.7,
+            },
+            'atom_blocks_kwargs': {
+                'onsite_levels_scale' : 0.5,
+                'select_orbitals' : {'Fe': 'd', 'Mo' : 'd'}
+            },
+        },
+        'calculator_options':{
+            'moments' : globalmoments,
+            'scfsteps': 10
+        }
+    },
+}
+cutoff = 'table'
+atoms = ['initial', 'relaxed']
+retry = False
 
-# only loop over the models
-#for keymodel, keyatoms, keycutof, moments in product(models.keys(), atoms.keys(), cutoffs.keys(), nmoments):
-atomspickle =  os.path.join(dataset,'Atomsobjects',  f'{components}-POSCAR-{atoms}-rescaled-AtomsObjects.pkl')
+
+# In[5]:
+
+target_case = 'EF_nmhcp' # only for correlation plots
 
 
-# In[4]:
+# In[6]:
 
 
-AtomsObjects = pd.read_pickle(atomspickle).dropna(how='any')
+AtomsObjects = {'initial' : load_atoms_objects(dataset,case='POSCAR.initial', scaling='rescaled'),
+                'relaxed' : load_atoms_objects(dataset, case='POSCAR.relaxed-all', scaling='noscaled')
+               }
+BS = load_fully_curated_briefsummary(dataset)
 
-from tqdm.contrib.concurrent import process_map
-from copy import deepcopy
+# In[8]:
 
-def inspect(compound):
-    print(compound)
-    compcopy = deepcopy(compound)
-    return compcopy
 
-#result = process_map(inspect, AtomsObjects['atoms'].iteritems())
+
+# In[9]:
+
+P = Projections()
+C = Canonicalmodel()
+P.readbxmodels()
+P.get_bond_chunks()
+P.get_autobonds()
+P.get_all_onsite_levels()
+P.get_restructured_projections()
+
+
+# In[10]:
+def create_modelfile(acompound, target_model_filename, modelname='projections', element_pairs_kwargs={}, atom_blocks_kwargs={} ):
+    print(acompound)
+    if modelname is not 'canonical':
+        model_filename = P.save_abond_bx(acompound, return_filename=True,
+                                         modelname=modelname, 
+                                         element_pairs_kwargs=element_pairs_kwargs,
+                                         atom_blocks_kwargs=atom_blocks_kwargs)
+        print(model_filename)
+    else:
+        model_filename = C.base_canonical #f'models/W_canonical.bx'
+    shutil.copy(model_filename, target_model_filename)
+
+
+# In[11]:
+
+
+def replace_symbols(theatoms, replacements=None):
+    new_symbols = theatoms.get_chemical_symbols()
+    if replacements is not None:
+        for original, replacement in replacements.items():
+            new_symbols = [s.replace(original, replacement) for s in new_symbols]
+    new_atoms = theatoms.copy()
+    new_atoms.set_chemical_symbols(new_symbols)
+    return new_atoms
+
+# In[13]:
+
 
 results = {}
+resultspickle = {}
 
-for model in models:
-    modelsfile = os.path.join('models', f'{components}_{model}.bx')
-    print('atoms: ', atoms, 'model: ', model, '  cutoff: ', cutoff, ' moments:', moments)
-# only when using branch check mag from bopfoxfeaturizer
-#    resultspickle = os.path.join(dataset, 'Descriptors', f'paralell_{components}_{atoms}_{model}_{cutoff}_WUBIND_{moments}_mag.pkl')
-    resultspickle = os.path.join(dataset, 'Descriptors', f'parallel_{components}_{atoms}_{model}_{cutoff}_WUBIND_{moments}.pkl')
-    print(f'saving in {resultspickle}')
-    BOPC = BopfoxFeatures(
-            AtomsObjects['atoms'],modelsfile, modelname=model,
+# In[14]:
+
+for (model, definition), (case, atoms_df) in product(model_definitions.items(), AtomsObjects.items()):
+    if 'moments' in definition.keys():
+        thismoments = definition['moments']
+    else:
+        thismoments = 16
+    if (model, thismoments, case) in results.keys():
+        continue
+    create_model_options = {}
+    if 'model_maker_options' in definition.keys():
+        create_model_options.update(definition['model_maker_options'])
+    use_elements = copy.copy(elements)
+    if 'replace atoms' in definition.keys():
+        for realelement, targetelement in definition['replace atoms'].items():
+            use_elements = set([s.replace(realelement, targetelement) for s in use_elements])
+    components = ''.join(use_elements)
+    modelsfile = os.path.join('models', f'{dataset}-{components}_{model}.bx')
+    create_modelfile(use_elements,modelsfile, modelname=model, **create_model_options,   )
+    if 'replace atoms' in definition.keys():
+        ApplyOnAtoms = atoms_df['atoms'].apply(replace_symbols, replacements = definition['replace atoms'])
+    else:
+        ApplyOnAtoms = atoms_df.atoms
+    print('atoms: ', case, 'model: ', model, '  cutoff: ', cutoff, ' moments:', thismoments)
+    resultspickle[(model, thismoments, case)] = os.path.join(dataset, 'Descriptors', f'parallel_{dataset}_{case}_{model}_{cutoff}_WUBIND_{thismoments}.pkl')
+    if not os.path.exists(resultspickle[(model, thismoments, case)]):
+        cwd = os.getcwd()
+        BOPC = BopfoxFeatures(
+            ApplyOnAtoms,modelsfile, modelname=model,
             cutoffby=cutoff, 
-            binary = '/home/storage/fortimtb/CuadernoTrabajo/bopfox/src/bopfox',  #oldrepobopfox/src/bopfox_mpi',
-            moments = moments,
+            binary = os.path.join(cwd, 'dependencies', 'bopfox','src', 'bopfox'),
+            savelog=True,
+            **definition['calculator_options']
             )
-    BOPC.featurize_dataframe(input_pickle=resultspickle, output_pickle=resultspickle)
-    results[model] = BOPC.RESULTS
-    BOPC.RESULTS.to_pickle(resultspickle)
+        BOPC.featurize_dataframe(input_pickle=resultspickle, output_pickle=resultspickle, )
+        results[(model, thismoments, case)] = BOPC.RESULTS #pd.read_pickle(resultspickle[model]) 
+        results[(model, thismoments, case)].to_pickle(resultspickle[(model, thismoments, case)])
+    else:
+        results[(model, thismoments, case)] = pd.read_pickle(resultspickle[(model, thismoments, case)])
 
+
+#In[]
+np.set_printoptions(precision=2,linewidth=125)
+
+
+removenans = [result.dropna(inplace=True) for (model, moments, case), result in results.items()]
+
+
+examplemodel = ('0.7projections_0.5os', 16, 'initial')
+
+
+# In[34]:
+
+
+descriptorlocation = os.path.join(dataset, 'Descriptors' )
+CNListLocation = os.path.join(descriptorlocation,'CNList.pkl')
+CNList = pd.read_pickle(CNListLocation)
+
+
+
+resultscnav = {}
+specialcolumns =['U_bind','U_bond_atom', 'modelsfile', 'logfile']#, 'U_bond_atom_list'] 
+remake = False
+progress = tqdm(results.items())
+for (model, moments, case), model_result in progress: # tqdm(results.items()):
+    averaged_bop_file = os.path.join(descriptorlocation,'CNAV_'+os.path.basename(resultspickle[(model, moments, case)].replace('.pkl','.csv')))
+    if os.path.exists(averaged_bop_file) and not remake:
+        print('read file')
+        resultscnav[(model, moments, case)] = pd.read_csv(averaged_bop_file, index_col=0).astype(float)  # for some reason some values are objects
+    else:
+        progress.set_description(model)
+        columnstoexpand = model_result.columns.drop(specialcolumns, errors='ignore')
+        df = gf.array_expansions(model_result[columnstoexpand],columnstoexpand)# columnstoexpand)
+        ThisCoordination = CNList#[result.index]
+        df = gf.featurize_dataframe(df, ThisCoordination, featurizer=gf.cn_average)
+        shape_factors = gf.get_shape_factors(df)
+        selector = VarianceThreshold()
+        selector.fit(df)
+        transformed_df = df.loc[:, selector.get_support()].astype(np.single)
+        resultscnav[(model, moments, case)] = pd.concat([transformed_df, shape_factors, model_result[['U_bind', 'U_bond_atom']]], axis=1)  # for some reason some values are objects
+        resultscnav[(model, moments, case)].to_csv(averaged_bop_file)
 
