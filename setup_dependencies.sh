a65a0ec488b98961c308ba29f8523c6b4d22a59e
Mariano Forti <mariano.forti@icams.rub.de>
2026-04-06 16:17:54 +0200

    setup_dependencies

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -25,13 +25,18 @@ clone_or_pull() {
     fi
 }
 
-clone_or_pull "git@git.noc.ruhr-uni-bochum.de:fortimtb/bopdftprojections.git" "$DEPS_DIR/bopdftprojections"
 clone_or_pull "git@git.noc.ruhr-uni-bochum.de:fortimtb/bopfoxfeaturizer.git"  "$DEPS_DIR/bopfoxfeaturizer"
 clone_or_pull "https://github.com/ICAMS/python-ace.git"                        "$DEPS_DIR/python-ace"
 clone_or_pull "https://github.com/AIIMProject/PyCEF.git"                       "$DEPS_DIR/PyCEF"             "packaging"
 
 # Optional: BopFox Fortran binary (requires manual compilation after cloning)
+# Uncomment following lines if you have access to these packages.
+#
+# clone_or_pull "git@git.noc.ruhr-uni-bochum.de:fortimtb/bopdftprojections.git" "$DEPS_DIR/bopdftprojections"
 # clone_or_pull "git@git.noc.ruhr-uni-bochum.de:fortimtb/bopfox.git" "$DEPS_DIR/bopfox"
+# cd $DEPS_DIR/bopfox 
+# make 
+# make install /src/bopfox/api
 
 echo ""
 echo "Done. Now run:  pip install -r requirements.txt"
