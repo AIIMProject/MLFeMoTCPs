aca4ece42247eed5bb20ce0ea892d42bb03b530e
Mariano Forti <mariano.forti@icams.rub.de>
2026-04-10 11:27:39 +0200

    test for resultsfiles

--- test/test_outputs.py
+++ test/test_outputs.py
@@ -22,19 +22,23 @@ def run_notebook(nbmake, notebook, env=None):
 @pytest.mark.order(1)
 def test_03(nbmake):
     run_notebook(nbmake, "03_PrepareDataset.ipynb")
+    assert os.path.exists("Fe-Mo/FullyCuratedParsedBriefSummary.json")
 
 
 # ---- 04 ----
 @pytest.mark.order(2)
 def test_04(nbmake):
     run_notebook(nbmake, "04_ComputeACEFeatures.ipynb")
+    assert os.path.exists("Fe-Mo/Descriptors/Fe-Mo-ACE-CNAV.csv")
     run_notebook(nbmake, "04_ComputeLibraryFeatures.ipynb")
+    assert os.path.exists("Fe-Mo/Descriptors/soap_features__specific__r_cut_4__n_max_6__l_max_5__sigma_0.1__rbf_gto__periodic_True.csv")
 
 
 # ---- 05 (special env) ----
 @pytest.mark.order(3)
 def test_05(nbmake):
     run_notebook(nbmake,"05_ComputeBOPFeatures.ipynb")
+    assert os.path.exists("Fe-Mo/Descriptors/CNAV_parallel_Fe-Mo_relaxed_0.7projections_0.5os_table_WUBIND_16.csv")
 
 
 # ---- 07 (multiple configs) ----
@@ -49,7 +53,12 @@ def test_07(nbmake, MODELNAME):
     run_notebook(nbmake, "07_MachineLearn-ModelSelection.ipynb",
         env={"MODELNAME": MODELNAME, "SKIP_IMPORTANCES": "SKIP"},
     )
+    assert os.path.exists("Fe-Mo/results/voting_regressor_KernelRidge.pkl")
+    assert os.path.exists("Fe-Mo/results/voting_regressor_MLP.pkl")
+    assert os.path.exists("Fe-Mo/results/voting_regressor_RandomForest.pkl")
+    assert os.path.exists("Fe-Mo/graphs/Figure_Fe-Mo_VotedRegressor_EF_nmhcp_KernelRidge.pdf")
     run_notebook(nbmake, "07_MachineLearn.ipynb")
+    assert os.path.exists("Fe-Mo/graphs/Fe-Mo_CNAV_only.pdf")
 
 
 # ---- 08 ----
